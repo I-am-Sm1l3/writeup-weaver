89ec3a04fed5287407d0bfb1e150e3cf332667ef
Sm1l3 <170519015+I-am-Sm1l3@users.noreply.github.com>
2025-12-24 09:20:48 +0500

    Add syntax highlighting for multiple languages

--- src/components/markdown-preview.tsx
+++ src/components/markdown-preview.tsx
@@ -3,6 +3,12 @@
 import MarkdownIt from 'markdown-it';
 import * as emoji from 'markdown-it-emoji';
 import { useMemo } from 'react';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-yaml';
 
 interface MarkdownPreviewProps {
   content: string;
@@ -15,6 +21,14 @@ export function MarkdownPreview({ content }: MarkdownPreviewProps) {
         html: true,
         linkify: true,
         typographer: true,
+        highlight: (str, lang) => {
+            if (lang && Prism.languages[lang]) {
+                try {
+                    return `<pre class="language-${lang}"><code>${Prism.highlight(str, Prism.languages[lang], lang)}</code></pre>`;
+                } catch (__) {}
+            }
+            return `<pre class="language-text"><code>${md.utils.escapeHtml(str)}</code></pre>`;
+        }
       }).use(emoji.full),
     []
   );
